1e3b49dc0a28cc62088bd8486a7bf0bcca24a203
Harsha-Reddy <43820092+HarshaReddyVardhan@users.noreply.github.com>
2025-05-18 21:49:03 -0500

    Updated with the new method for this problem.

--- hashset.java
+++ hashset.java
@@ -7,7 +7,7 @@
 // This solution is 10^6 * 1 byte for every boolean value.
 
 
-
+// Previous Method
 int size = 1000001;
 	boolean[] flag;
     public MyHashSet() {
@@ -25,3 +25,62 @@ int size = 1000001;
     public boolean contains(int key) {
         return flag[key];
     }
+
+//New Method added
+
+Time Complexity : O(1)
+// Space Complexity : O(N) -- that is 10^6 because that is the max case
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : Coming up whith this solution was hard. With help of instructor i was able to learn this meghtond and implement.
+class MyHashSet {
+    private boolean [][] storage;
+    private int bucketSize;
+    private int bucketItemSize;
+
+    private int hashKey1(int key){
+        return key%1000;
+    }
+    private int hashKey2(int key){
+        return key/1000;
+    }
+
+    public MyHashSet() {
+        this.bucketSize = 1000;
+        this.bucketItemSize = 1000;
+        this.storage = new boolean[bucketSize][];
+    }
+    
+    public void add(int key) {
+        int firstHash = hashKey1(key);
+        if(storage[firstHash] == null){
+            if(firstHash == 0)
+                storage[firstHash] = new boolean[bucketItemSize+1];
+            else
+                storage[firstHash] = new boolean[bucketItemSize];
+        }
+        int secondHash = hashKey2(key);
+        storage[firstHash][secondHash] = true;
+    }
+    
+    public void remove(int key) {
+        int firstHash = hashKey1(key);
+        if(storage[firstHash] == null) return;
+        int secondHash = hashKey2(key);
+        storage[firstHash][secondHash] = false;
+    }
+    
+    public boolean contains(int key) {
+        int firstHash = hashKey1(key);
+        if(storage[firstHash] == null) return false;
+        int secondHash = hashKey2(key);
+        return storage[firstHash][secondHash];
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
